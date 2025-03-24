e88c3631ef0e31c1516adaa68bc23d3bf794da56
Dimitris Stasinos <staa.dista@yahoo.gr>
2025-03-24 21:52:47 +0200

    update

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ import questionary
 import subprocess
 import sys
 
+def executeScript(script, directory):
+
+    process = subprocess.Popen([sys.executable, "-u", script], cwd=directory,
+        stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+
+    for line in process.stdout:
+        print(line, end="")
+
+    process.wait()
 
 while True:
     select = questionary.select(
-        "Select:",
+        "Select a choice",
         choices=[
             "MLP K-Fold Cross-Validation",
             "MLP Train",
@@ -14,16 +23,10 @@ while True:
     ).ask()
 
     if select == "MLP K-Fold Cross-Validation":
-        print("Executing MLP K-Fold Cross-Validation...\n")
-        result = subprocess.run([sys.executable, "MLP_K_Fold.py"], cwd="Deep_Neural_Networks/MLP",
-        capture_output=True,text=True)
-        print(result.stdout)
+        executeScript("MLP_K_Fold.py", "Deep_Neural_Networks/MLP")
 
     elif select == "MLP Train":
-        print("Executing MLP model training...\n")
-        result = subprocess.run([sys.executable, "MLP.py"], cwd="Deep_Neural_Networks/MLP",
-        capture_output=True,text=True)
-        print(result.stdout)
+        executeScript("MLP_Train.py", "Deep_Neural_Networks/MLP")
 
     elif select == "Exit":
         break
